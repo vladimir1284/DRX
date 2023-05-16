735945f9dbc5d1208b82be664cb6bd41150c7054
Yosel de Jesús Balibrea Lastre <yosel.balibrea@reduc.edu.cu>
2023-05-16 01:32:18 -0400

    new data gen

--- Vivado/src/data_gen/data_gen.vhd
+++ Vivado/src/data_gen/data_gen.vhd
@@ -1,78 +1,162 @@
+-- Generador de Datos IQ
+-- Yosel de Jesus Balibrea Lastre
+-------------------------------------
+
+
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 
--------------------------------------------------------------------------------
--- entity declaration
--------------------------------------------------------------------------------
 entity data_gen is
-generic(
-    AT : integer := 2000;
-    DT : integer := 1000;
-    CLK_FREQ : integer := 100000;
-    W : integer := 32
-);
+    generic (
+        CLK_FREQ : integer := 100000000
+    );
+    port (
+        clk   : in std_logic;
+        rst   : in std_logic;
+        en    : in std_logic;
 
-port(
-    clk : in std_logic;
-    rst : in std_logic;
-    en  : in std_logic;
-    data_o : out std_logic_vector(W-1 downto 0)
-);
-end entity data_gen;
+        data_o : out std_logic_vector(63 downto 0);
+        clk_fifo : out std_logic;
+        
+        pulso  : in std_logic;
+        samples : in std_logic_vector(31 downto 0);
+        freq    : in std_logic_vector(31 downto 0)
+    );
+end entity;
 
 
 architecture rtl of data_gen is
-    type fsm_type is (SEND_S, WAIT_S, IDLE_S);
 
-    signal state : fsm_type;
+    -- Numero Magico
+    constant MAG_NUM : std_logic_vector(63 downto 0) := X"CAFEFFFFFFFFFFFF";
+    
 
-    signal counter : integer;
-    signal wcnt : integer;
+    -- Periodos de reloj en pulso largo y corto
+    constant PULSO_COTRO : integer := 41;   -- 1.2MHz (100/(2* 1.2))
+    constant PULSO_LARGO : integer := 82;  -- 600KHz
+    signal clk_reg : integer := 0;
+
+    -- Profundidad del Header
+    constant HDEPTH : integer := 2;
+    
+    -- Numero del rayo
+    signal ray_cnt : std_logic_vector(63 downto 0) := (others => '0') ;    
+    
+    -- Azimut y elevacion
+    signal azelev_c : std_logic_vector(31 downto 0) := (others => '0');
+
+    -- Contador IQ
+    signal iq_cnt  : std_logic_vector(15 downto 0) := (others => '0');  
+
+    -- Numero de muestra
+    signal index  : integer := 0; 
+
+    signal clk_count : integer := 0;    -- CLK FIFO
+
+    signal clk_fifo_r: std_logic := '0';
+
+    -- Reg de datos
+    signal data_reg : std_logic_vector(63 downto 0) := (others => '0') ;
+    
+    
+    -- FSM
+    type fsm_type is (IDLE_S, HEADER_S, IQ_S);
+    signal fsm : fsm_type;
+
 begin
 
-process(clk)
-begin
 
-    if rising_edge(clk) then
+    -- CLK FIFO GEN ------------------------------------------------
+    process (clk)
+    begin
         if rst = '0' then
-            state <= IDLE_S;
-        else
-            case state is
+            clk_fifo_r <= '0';
+            clk_reg <= 0; 
+
+        elsif rising_edge(clk) then
+            if pulso = '1' then
+                clk_reg <= PULSO_COTRO;
+            else
+                clk_reg <= PULSO_LARGO;
+            end if;
+
+            clk_count <= clk_count + 1;
+            if clk_count >= clk_reg then
+                clk_fifo_r <= not clk_fifo_r;
+                clk_count <= 0;
+            end if; 
+
+        end if;            
+        
+    end process;
+
+    clk_fifo <= clk_fifo_r;
+    --------------------------------------------------------------------
+
+
+    -- FSM -------------------------------------------------------------
+    process (clk)
+    begin
+
+        if rst = '0' then
+            azelev_c <= (others => '0');
+            iq_cnt <= (others => '0');
+            index <= 0;
+            ray_cnt <= (others => '0');
+
+        elsif rising_edge(clk) then
+            
+            case fsm is
                 when IDLE_S =>
-                    
-                    counter <= AT * CLK_FREQ/1000;
-                    wcnt <= DT * CLK_FREQ/1000;
+
+                    --azelev_c <= (others => '0');
+                    iq_cnt <= (others => '0');
+                    index <= 0;
 
                     if en = '1' then
-                        state <= SEND_S;
+                        fsm <= HEADER_S;
                     end if;
 
-                when SEND_S =>                    
-
-                    if counter = 0 then
-                        state <= WAIT_S;
-                    end if;
-                    -- Send data
-                    counter <= counter - 1;
-
-                when WAIT_S =>
+                when HEADER_S =>
+                    index <= index + 1;
                     
-                    if wcnt = 0 then
-                        state <= IDLE_S;
+                    case index is
+                        when 0 =>
+                            data_reg <= MAG_NUM;
+
+                        when 1 =>
+                            data_reg <= ray_cnt;
+                            ray_cnt <= std_logic_vector(unsigned(ray_cnt) + 1);
+
+                        when 2 =>
+                            data_reg <= azelev_c & azelev_c;
+                            azelev_c <= std_logic_vector(unsigned(azelev_c) + 1);
+
+                        when others =>
+
+                    end case;
+
+                    if index >= HDEPTH then
+                        index <= 0;
+                        fsm <= IQ_S;
                     end if;
+                        
+                when IQ_S =>
+                    index <= index + 1;
+                    iq_cnt <= std_logic_vector(unsigned(iq_cnt) + 1);
 
-                    wcnt <= wcnt - 1;
+                    data_reg <= iq_cnt & iq_cnt & iq_cnt & iq_cnt;
 
+                    if index >= to_integer(unsigned(samples)) then
+                        fsm <= IDLE_S;
+                    end if;                    
+                    
             end case;
-
         end if;
+    end process;
 
-    end if;
+    data_o <= data_reg;    
 
-end process;
-
-    --data_o <= std_logic_vector(to_unsigned(counter, W));
-
-end rtl;
+end architecture;
--- Vivado/src/data_gen/data_gen_tb.vhd
+++ Vivado/src/data_gen/data_gen_tb.vhd
@@ -2,61 +2,71 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-
--------------------------------------------------------------------------------
--- entity declaration
--------------------------------------------------------------------------------
 entity data_gen_tb is
+end;
 
-end entity data_gen_tb;
+architecture bench of data_gen_tb is
 
+  component data_gen
+    generic (
+      CLK_FREQ : integer
+    );
+      port (
+      clk : in std_logic;
+      rst : in std_logic;
+      en : in std_logic;
+      data_o : out std_logic_vector(63 downto 0);
+      clk_fifo : out std_logic;
+      pulso : in std_logic;
+      samples : in std_logic_vector(31 downto 0);
+      freq : in std_logic_vector(31 downto 0)
+    );
+  end component;
 
-architecture rtl of data_gen_tb is
+  -- Clock period
+  constant clk_period : time := 10 ns;
+  -- Generics
+  constant CLK_FREQ : integer := 100000000;
 
-    constant CLK_PERIOD_NS : time := 20 ns;
+  -- Ports
+  signal clk : std_logic;
+  signal rst : std_logic;
+  signal en : std_logic;
+  signal data_o : std_logic_vector(63 downto 0);
+  signal clk_fifo : std_logic;
+  signal pulso : std_logic;
+  signal samples : std_logic_vector(31 downto 0);
+  signal freq : std_logic_vector(31 downto 0);
 
-    signal clk          : std_logic := '1';
-    signal rst          : std_logic;
-    signal en : std_logic;
-    signal data_o : std_logic_vector(7 downto 0);
-    
 begin
 
-    -- clock generation
-    Clk <= not Clk after CLK_PERIOD_NS/2;
+  pulso <= '0';
+  rst <= '1';
+  en <= '1';
 
-    dg: entity work.data_gen
-    generic map(
-        AT => 20,
-        DT => 10,
-        CLK_FREQ => 50000,
-        W => 8
+  samples <= X"00000064";
+
+  data_gen_inst : data_gen
+    generic map (
+      CLK_FREQ => CLK_FREQ
     )
-    port map(
-        clk => clk,
-        rst => rst,
-        en => en,
-        data_o => data_o
+    port map (
+      clk => clk,
+      rst => rst,
+      en => en,
+      data_o => data_o,
+      clk_fifo => clk_fifo,
+      pulso => pulso,
+      samples => samples,
+      freq => freq
     );
 
-    wave_gen: process
-    begin
-        rst <= '0';
-        en <= '1';
+   clk_process : process
+   begin
+   clk <= '1';
+   wait for clk_period/2;
+   clk <= '0';
+   wait for clk_period/2;
+   end process clk_process;
 
-        wait for (4 * CLK_PERIOD_NS);
-
-        rst <= '1';
-        en <= '0';
-
-        wait for (4 * CLK_PERIOD_NS);
-
-        en <= '1';
-        rst <= '1';
-
-        wait;
-
-    end process;
-
-
-end rtl;
+end;
